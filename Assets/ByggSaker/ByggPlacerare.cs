e32d6d6136cdd2f4b5cd91f2c1bc2695ada0f4fc
Leon <leon.karkkainenlind@elev.ga.klaragymnasium.se>
2024-09-23 16:21:22 +0200

    fungerande byggsystem utan byggnader
    2 st just nu (en vit och röd fyrkant)
    
    simon for rita dem och fixa resten tycker jag

--- Assets/ByggSaker/ByggPlacerare.cs
+++ Assets/ByggSaker/ByggPlacerare.cs
@@ -1,35 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ByggPlacerare : MonoBehaviour
 {
     public Grid grid; // Vilken grid
     public GameObject buildingPrefab; // Vilken byggnad
-    public SpookyBuilding spookyBuildings;
+    public ByggValet aktivByggnad; // aktiv byggnad via annan skript
+    public SpookyBuilding deleteremovebanish;
 
-    /*
-    private void Awake()
-    {
-        
-        buildingPrefab = GameObject.FindGameObjectWithTag("").transform;
-    }
-    */
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !IntePlaceraVidKnappar())
         {
             Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Tar musens position
             Vector3Int gridPosition = grid.WorldToCell(mouseWorldPos); // Konverterar musen till cell/grid
             Vector3 finalPosition = grid.CellToWorld(gridPosition); // Grid snappar byggnaden till närmsta cell (till musen)
             finalPosition.z = -8;
 
+            // välj byggnad
+            GameObject buildingPrefab = aktivByggnad.getAktivByggnad();
+
             // Bygg
             Instantiate(buildingPrefab, finalPosition, Quaternion.identity);
 
-            // göm spöket
-            //spookyBuildings.noHalloween();
+            
         }
+        if (Input.GetMouseButtonDown(1))
+        {
+            gameObject.active = false;
+
+            deleteremovebanish.hejhejpreview(); // va inte roligt att fixa det här
+        }
+    }
+
+    private bool IntePlaceraVidKnappar()
+    {
+        return EventSystem.current.IsPointerOverGameObject();
     }
 }
